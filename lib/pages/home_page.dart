206fc3472258ed6fb23870df1adccd261250f903
Vikrant-Vasu <vikrantvasu.it@gmail.com>
2022-09-02 22:41:26 +0530

    day 10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,19 +4,19 @@ import '../widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-   final  int days = 30;
-   final String name = "Codepur";
+  final int days = 30;
+  final String name = "Codepur";
 
   @override
   Widget build(BuildContext context) {
-     
     return Scaffold(
       appBar: AppBar(
-
         title: Text("Catalog App"),
       ),
       body: Center(
-        child: Text("Welcome to $days days of flutter $name"),
+        child: Container(
+          child: Text("Welcome to $days days of flutter $name"),
+        ),
       ),
       drawer: MyDrawer(),
     );
