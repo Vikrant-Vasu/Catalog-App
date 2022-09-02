fd07466c72f845c59432e3dd5bccf2b4755fa398
Vikrant-Vasu <vikrantvasu.it@gmail.com>
2022-09-02 22:25:47 +0530

    themes extracted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,15 +5,15 @@ import '../widgets/drawer.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
    final  int days = 30;
-
-    final String name = "Codepur";
+   final String name = "Codepur";
 
   @override
   Widget build(BuildContext context) {
      
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog App"),
+
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Text("Welcome to $days days of flutter $name"),
