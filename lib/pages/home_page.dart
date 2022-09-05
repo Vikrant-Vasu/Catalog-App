c0867281c20736d571fc8ef098e2f5f3c13e24a8
Vikrant-Vasu <vikrantvasu.it@gmail.com>
2022-09-05 22:10:13 +0530

    day 11 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text("Welcome to $days days of flutter $name"),
+          child: Text(context.runtimeType.toString()),
         ),
       ),
       drawer: MyDrawer(),
